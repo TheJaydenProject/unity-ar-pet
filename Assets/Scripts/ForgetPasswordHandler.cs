aac905dc8e25a974aff5095f8f0e20990538a0ed
Ellysha Adriana Puteri Mohamad Yusof <S10267273@connect.np.edu.sg>
2025-12-11 00:52:20 +0800

    UI added
    For Login and homepage (more to be added?)

---
+++ Assets/Scripts/ForgetPasswordHandler.cs
@@ -0,0 +1,17 @@
+// Author: Ellysha Adriana
+// Date: 11 December 2025
+// Desc: Controls Forget Password button 
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ForgotPasswordHandler : MonoBehaviour
+{
+    public void OnForgotPasswordClicked()
+    {
+        Debug.Log("Forgot Password clicked!");
+        // firebase script can go here
+        // Example: Open password reset UI
+        // SceneManager.LoadScene("PasswordResetScene");
+    }
+}
